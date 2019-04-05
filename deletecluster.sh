f69376dceafe6d7a63a40b9e7ec6d36b427f0bcc
Reza Rahim <rezarahim@Rezas-MacBook-Pro.local>
2019-04-06 01:16:29 +0600

    build

---
+++ deletecluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export count=3
+export type=reza-kubeadm
+
+for ((i=1; i<=$count; i++));
+do
+printf "deleting $type-$i ..."
+yes | gcloud compute --project=central-beach-194106 instances delete $type-$i --zone=us-east1-b &
+done

0606c4efa464e6edc67b5a82c8d82b873d6a372f
Your Name <you@example.com>
2019-04-05 00:29:46 +0000

    first commit

---
+++ docker.sh
@@ -0,0 +1,13 @@
+sudo apt -y update
+
+sudo apt-get -y install  apt-transport-https  ca-certificates  curl  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key -y fingerprint 0EBFCD88
+
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+
+sudo apt-get -y  update
+
+sudo apt-get -y install docker-ce
